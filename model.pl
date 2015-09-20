139bdec34fe59c1f41c2dfa6f765c33155f0a669
Jonas van Oenen <jonas.vanoenen@gmail.com>
2015-09-20 21:05:14 +0200

    Dingen geprobeerd

--- model.pl
+++ model.pl
@@ -1,13 +1,36 @@
 :- [reassert].
 :- dynamic val/3.
 
-% Components of all gates.
+% Components of model 1
 component(m1, mul, a, c, x).
 component(m2, mul, b, d, y).
 component(m3, mul, c, e, z).
 component(a1, add, x, y, f).
 component(a2, add, y, z, g).
 
+%% Abstract sketch of model 2
+%% 
+%% a3 -> m4 - \
+%% a4 -> m5 - -> a7 -> r
+%% a5 -> m6 - -> a8 -> s
+%% a6 -> m7 - /
+%% 
+
+%% Components of model 2
+component(a3, add, a, b, i).
+component(a4, add, c, d, j).
+component(a5, add, e, f, k).
+component(a6, add, g, h, l).
+
+component(m4, mul, i, j, u).
+component(m5, mul, j, k, o).
+component(m6, mul, k, j, p).
+component(m7, mul, k, l, q).
+
+component(a7, add, u, o, r).
+component(a8, add, p, q, s).
+
+%% General priori rates for multipliers and adders
 failure_rate(C, 0.0006) :-
 	component(C, mul, _, _, _).
 failure_rate(C, 0.0004) :-
@@ -20,18 +43,28 @@ output_var(X) :-
 		\+ component(_, _, _, X, _)
 	).
 
-% Model 1 (example for forward chaining)
+% Model 1, Input values(example for forward chaining)
 val(1, a, 5).
 val(1, b, 8).
 val(1, c, 10).
 val(1, d, 15).
 val(1, e, 8).
 
-% Model 2 (example for backward chaining)
-val(2, f, 170).
-val(2, g, 200).
+% Model 3, Output values(example for backward chaining)
+val(3, f, 170).
+val(3, g, 200).
 
-% Inner logic of the gates, or "inner gate predicates".
+% Model 2, Input values (Example with 10 components)
+val(2, a, 9).
+val(2, b, 12).
+val(2, c, 8).
+val(2, d, 6).
+val(2, e, 2).
+val(2, f, 10).
+val(2, g, 16).
+val(2, h, 5).
+
+% The defined adder and multiplier components
 add(X, Y, Z) :-
 	Z is X + Y.
 mul(X, Y, Z) :-
@@ -110,7 +143,7 @@ backward(M) :-
 	% Construct an inner gate predicate.
 	Predicate =.. [Type, X1Value, X2Value, YValue],
 	
-	% Check if the values of the input variables X1 and X2 match up to the value of the output variable Y.
+	% Check if the values of the input variables X1 and X2 match up to the value of the output variable Y
 	call(Predicate),
 
 	% Assert the derived values of the input variables X1 and X2 as new facts in model M.
@@ -151,10 +184,54 @@ measure_var(X, Input) :-
 	read(Input),
 	write_ln(' ').
 
-find_highest_faulty_comp(Output, InputComp)
+% find the component linked to the specified output varianle
+findComp(OutputVar, Comp):-
+	component(Comp, _, _, _, OutputVar).
+
+findFaultyComp([], Answer):-
+	write('The faulty component is '),
+	writeln(Answer).
+
+%% Find most likely faulty component set
+findFaultyCompSet([[OutputVar|_]|_]):-
+	%%  Find the set with Highest chance of fault
+	findHighestPrioriSet(OutputVar, 0, SetOfComp),
+	%% Test for the faulty components set
+	testFaultyComp(SetOfComp, []).
+
+% Faulty component is found
+testFaultyComp([], FaultyVariable):-
+	write('Faulty component is '),
+	writeln(FaultyVariable).
+
+% Test for finding faulty component
+testFaultyComp([H|T], FaultyVariables):-
+	%%  find the input for first component of probable faulty set
+	component(H, _, Input1, Input2, _),
+	%% If no faulty variable go to next component in probable faulty set
+	faulty_vars(1, [Input1,Input2], FaultyVariables),
+	testFaultyComp(T, FaultyVariables).
+
+%% Find set of components with Highest faulty rate (RETURNED OP DIT MOMENT EEN LEGE SET, moet anders)
+findHighestPrioriSet(X, Rate, Set):-
+	member(X,[a,b,c,d,e]),
+	write('Highest failure rate found on set '),
+	write(Set),
+	write(' with rate '),
+	writeln(Rate).
+
+% METHODE VIND NU NOG ALLEEN DE MEEST LINKER SET
+findHighestPrioriSet(Start, Rate, Set):-
+	component(Comp, _, X, _, Start),
+	failure_rate(Comp, FR),
+	NewRate is Rate + FR,
+	append([Comp], Set, NewSet),
+	findHighestPrioriSet(X, NewRate, NewSet).
 
 go(M) :-
 	\+ forward(M),
 	findall(X, output_var(X), OutputVariables),
 	faulty_vars(M, OutputVariables, FaultyVariables),
-	print(FaultyVariables).
+	print(FaultyVariables),
+	findFaultyCompSet(FaultyVariables).
+	
